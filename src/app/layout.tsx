0cbf1df20841e9ee82f0d96efc20410db2efab88
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-07 00:11:43 +0100

    Add Google Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import '../lib/css/main.css';
 import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
 import { FC, ReactNode } from 'react';
 import background from '../../public/background.svg';
 import { NavigationBar } from '../components/server';
 import { rootTitle } from '../lib/constants';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: rootTitle,
@@ -24,7 +32,12 @@ interface Props {
 const RootLayout: FC<Props> = async props => {
   return (
     <html lang='en'>
-      <body style={{ backgroundImage: `url(${background.src})` }}>
+      <body
+        style={{
+          backgroundImage: `url(${background.src})`,
+          fontFamily: poppins.style.fontFamily,
+        }}
+      >
         <NavigationBar />
         {props.children}
       </body>
