32c332eab7b791407ae88a37593e4dd53acb48f1
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-06 14:08:47 +0100

    Use Tailwind classes in example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@ import { FC } from 'react';
 import { Foo } from '../components';
 
 const HomePage: FC = () => {
-  return <Foo suffix='bar' />;
+  return (
+    <Foo
+      suffix='bar'
+      className='absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2'
+    />
+  );
 };
 
 export default HomePage;
--- src/components/foo.tsx
+++ src/components/foo.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 
 interface Props {
+  className?: string;
   suffix: string;
 }
 
-const Foo: FC<Props> = props => <div>Foo {props.suffix}</div>;
+const Foo: FC<Props> = props => <div className={props.className}>Foo {props.suffix}</div>;
 
 export default Foo;
