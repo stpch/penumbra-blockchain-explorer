60db0e8191115792e8c8ffd5cd9d8a4f5ffb4121
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-06 14:51:32 +0100

    Set up static deployment to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // basePath: process.env.NODE_ENV === 'production' ? '/deployment' : '',
+  output: 'export',
 };
 
 export default nextConfig;
