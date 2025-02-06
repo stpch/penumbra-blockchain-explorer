0652e4f1b97aefc92ce0d55c9e048ea7b1d4cd79
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-06 22:28:23 +0100

    Configure CSS modules with PostCSS

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,12 +2,6 @@ export default {
   extends: ['stylelint-config-recommended', 'stylelint-config-idiomatic-order'],
   plugins: ['stylelint-prettier'],
   rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen'],
-      },
-    ],
     'no-descending-specificity': null,
     'prettier/prettier': [true, { singleQuote: false }],
   },
