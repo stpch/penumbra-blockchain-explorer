f387070ef3511ded73c3e5ccc0faf154b19e6378
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-07 04:06:33 +0100

    Implement tab indicator using only CSS

--- src/components/navigationBar/navigationBar.tsx
+++ src/components/navigationBar/navigationBar.tsx
@@ -5,7 +5,6 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { FC, ReactNode } from 'react';
 import nextConfig from '../../../next.config';
-import Container from '../container';
 import { Tab, Tabs } from '../tabs';
 import styles from './navigationBar.module.css';
 
@@ -15,21 +14,15 @@ interface Props {
 }
 
 const NavigationBar: FC<Props> = props => (
-  <Container>
-    <header className={clsx(styles.root, props.className)}>
-      <Image src={`${nextConfig.basePath}/logo.png`} alt='Noctis' width={120} height={33} />
-      <Tabs>
-        {onTabActive => (
-          <>
-            <Tab href='/' label='Home' onActive={onTabActive} />
-            <Tab href='/blocks' label='Blocks' onActive={onTabActive} />
-            <Tab href='/transactions' label='Transactions' onActive={onTabActive} />
-          </>
-        )}
-      </Tabs>
-      <div>TODO: Search and price</div>
-    </header>
-  </Container>
+  <header className={clsx(styles.root, props.className)}>
+    <Image src={`${nextConfig.basePath}/logo.png`} alt='Noctis' width={120} height={33} />
+    <Tabs>
+      <Tab href='/' label='Home' />
+      <Tab href='/blocks' label='Blocks' />
+      <Tab href='/transactions' label='Transactions' />
+    </Tabs>
+    <div>TODO: Search and price</div>
+  </header>
 );
 
 export default NavigationBar;
--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -2,54 +2,19 @@
 'use client';
 
 import clsx from 'clsx';
-import { FC, ReactElement, useCallback, useRef } from 'react';
+import { FC, ReactElement } from 'react';
 import { Props as TabProps } from './tab';
 import styles from './tabs.module.css';
 
 interface Props {
-  children?: (
-    onTabActive: (label: string) => void,
-  ) =>
+  children?:
     | ReactElement<TabProps>
     | Array<ReactElement<TabProps> | Array<ReactElement<TabProps>> | false | null | undefined>;
   className?: string;
 }
 
-const Tabs: FC<Props> = props => {
-  const root = useRef<HTMLDivElement>(null);
-  const indicator = useRef<HTMLDivElement>(null);
-
-  const onTabActive = useCallback((label: string) => {
-    if (!root.current || !indicator.current) {
-      return;
-    }
-
-    const tab = Array.from(root.current.childNodes).find(node => node.textContent === label) as
-      | HTMLElement
-      | undefined;
-
-    if (tab) {
-      indicator.current.style.transform = `translateX(${tab.offsetLeft}px)`;
-      indicator.current.style.width = `${tab.offsetWidth}px`;
-
-      if (indicator.current.style.height) {
-        return;
-      }
-
-      setTimeout(() => {
-        if (indicator.current) {
-          indicator.current.style.height = '2px';
-        }
-      }, 200);
-    }
-  }, []);
-
-  return (
-    <nav className={clsx(styles.root, props.className)} ref={root}>
-      <div ref={indicator} className={styles.indicator} />
-      {typeof props.children === 'function' && props.children(onTabActive)}
-    </nav>
-  );
-};
+const Tabs: FC<Props> = props => (
+  <nav className={clsx(styles.root, props.className)}>{props.children}</nav>
+);
 
 export default Tabs;
