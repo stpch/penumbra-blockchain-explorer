611fdd3af5ce80a63d3fa67c04884bcdd29a3154
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-09 00:19:50 +0100

    Style table and add dashboard mock data

--- src/components/server/dashboard/dashboard.tsx
+++ src/components/server/dashboard/dashboard.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 import { FC, ReactNode } from 'react';
 import { BlockPanel, BurnPanel, TransactionPanel } from '../../client';
 import Table from '../table';
@@ -14,11 +15,127 @@ const Dashboard: FC<Props> = props => (
     <BlockPanel />
     <TransactionPanel />
     <BurnPanel />
-    <Table title='Latest blocks' actions='View all' footer='Footer'>
-      Latest blocks table
+    <Table title='Latest blocks' actions={<Link href='/blocks'>View all</Link>}>
+      <thead>
+        <tr>
+          <th>Block height</th>
+          <th>Time</th>
+          <th>Txs</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1,057,456</td>
+          <td>1s ago</td>
+          <td>7</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>3s ago</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>10min ago</td>
+          <td>12</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>12min ago</td>
+          <td>5</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>3h ago</td>
+          <td>4</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>3hr ago</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>5hr ago</td>
+          <td>1</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>12hr ago</td>
+          <td>7</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>1d ago</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>1,057,456</td>
+          <td>1wk ago</td>
+          <td>15</td>
+        </tr>
+      </tbody>
     </Table>
-    <Table title='Latest transactions' actions='View all' footer='Footer'>
-      Latest translations table
+    <Table title='Latest transactions' actions={<Link href='/transactions'>View all</Link>}>
+      <thead>
+        <tr>
+          <th>Tx hash</th>
+          <th>Block height</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>Swap claim</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>Undeledate claim</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>Withdraw from a Dutch auction</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>End a Dutch auction</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>Withdraw from a Dutch auction</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>End a Dutch auction</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>Undelegate</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>Delegate</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>Schedule a Dutch auction</td>
+        </tr>
+        <tr>
+          <td>f3a4b8d0...76a1c092</td>
+          <td>1,057,456</td>
+          <td>ics20 withdrawal</td>
+        </tr>
+      </tbody>
     </Table>
   </div>
 );
