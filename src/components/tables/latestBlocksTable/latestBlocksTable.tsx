e5e0a3142bc686b2c4e581a806c60120af2c2880
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-09 20:17:01 +0100

    Implement table row click and navigation

--- src/components/tables/latestBlocksTable/latestBlocksTable.tsx
+++ src/components/tables/latestBlocksTable/latestBlocksTable.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Box } from 'lucide-react';
-import { FC } from 'react';
+import { useRouter } from 'next/navigation';
+import { FC, MouseEvent, useCallback } from 'react';
 import { Block } from '../../../lib/types';
 import Button from '../../button';
 import Table from '../table';
@@ -9,33 +12,44 @@ interface Props {
   className?: string;
 }
 
-const LatestBlocksTable: FC<Props> = props => (
-  <Table
-    title='Latest blocks'
-    actions={<Button href='/blocks'>View all</Button>}
-    alignLastRight
-    className={props.className}
-  >
-    <thead>
-      <tr>
-        <th>Block height</th>
-        <th>Time</th>
-        <th>Txs</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.blocks.map(block => (
-        <tr key={block.id}>
-          <td>
-            <Box size={16} color='var(--textSecondary)' />
-            <span>{block.height}</span>
-          </td>
-          <td>{block.date.toLocaleTimeString()}</td>
-          <td>{block.transactions}</td>
+const LatestBlocksTable: FC<Props> = props => {
+  const router = useRouter();
+
+  const onRowClick = useCallback(
+    (e: MouseEvent<HTMLTableRowElement>) => {
+      router.push(`/blocks/${e.currentTarget.dataset.blockId}`);
+    },
+    [router],
+  );
+
+  return (
+    <Table
+      title='Latest blocks'
+      actions={<Button href='/blocks'>View all</Button>}
+      alignLastRight
+      className={props.className}
+    >
+      <thead>
+        <tr>
+          <th>Block height</th>
+          <th>Time</th>
+          <th>Txs</th>
         </tr>
-      ))}
-    </tbody>
-  </Table>
-);
+      </thead>
+      <tbody>
+        {props.blocks.map(block => (
+          <tr key={block.id} data-block-id={block.id} onClick={onRowClick}>
+            <td>
+              <Box size={16} color='var(--textSecondary)' />
+              <span>{block.height}</span>
+            </td>
+            <td>{block.date.toISOString()}</td>
+            <td>{block.transactions}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
 
 export default LatestBlocksTable;
--- src/components/tables/latestTransactionsTable/latestTransactionsTable.tsx
+++ src/components/tables/latestTransactionsTable/latestTransactionsTable.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Box, CheckCheck, Copy } from 'lucide-react';
-import { FC } from 'react';
+import { useRouter } from 'next/navigation';
+import { FC, MouseEvent, useCallback } from 'react';
 import { Transaction } from '../../../lib/types';
 import Button from '../../button';
 import Pill from '../../pill';
@@ -11,41 +14,52 @@ interface Props {
   className?: string;
 }
 
-const LatestTransactionsTable: FC<Props> = props => (
-  <Table
-    title='Latest transactions'
-    actions={<Button href='/transactions'>View all</Button>}
-    className={props.className}
-  >
-    <thead>
-      <tr>
-        <th>Tx hash</th>
-        <th>Block height</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.transactions.map(transaction => (
-        <tr key={transaction.id}>
-          <td>
-            <CheckCheck size={14} color='var(--secondaryLight)' />
-            <span>{transaction.hash}</span>
-            <Copy size={14} color='var(--textSecondary)' />
-          </td>
-          <td>
-            <Box size={16} color='var(--textSecondary)' />
-            <span>{transaction.blockHeight}</span>
-          </td>
-          <td>
-            <Pill>{transaction.latestAction}</Pill>
-            {transaction.totalActions > 1 && (
-              <span className={styles.moreActions}>+{transaction.totalActions - 1}</span>
-            )}
-          </td>
+const LatestTransactionsTable: FC<Props> = props => {
+  const router = useRouter();
+
+  const onRowClick = useCallback(
+    (e: MouseEvent<HTMLTableRowElement>) => {
+      router.push(`/transactions/${e.currentTarget.dataset.transactionId}`);
+    },
+    [router],
+  );
+
+  return (
+    <Table
+      title='Latest transactions'
+      actions={<Button href='/transactions'>View all</Button>}
+      className={props.className}
+    >
+      <thead>
+        <tr>
+          <th>Tx hash</th>
+          <th>Block height</th>
+          <th>Actions</th>
         </tr>
-      ))}
-    </tbody>
-  </Table>
-);
+      </thead>
+      <tbody>
+        {props.transactions.map(transaction => (
+          <tr key={transaction.id} data-transaction-id={transaction.id} onClick={onRowClick}>
+            <td>
+              <CheckCheck size={14} color='var(--secondaryLight)' />
+              <span>{transaction.hash}</span>
+              <Copy size={14} color='var(--textSecondary)' />
+            </td>
+            <td>
+              <Box size={16} color='var(--textSecondary)' />
+              <span>{transaction.blockHeight}</span>
+            </td>
+            <td>
+              <Pill>{transaction.latestAction}</Pill>
+              {transaction.totalActions > 1 && (
+                <span className={styles.moreActions}>+{transaction.totalActions - 1}</span>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
 
 export default LatestTransactionsTable;
