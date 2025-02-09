6ca7558c3870afba29965e1edf020d03c30cea39
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-09 16:04:15 +0100

    Add Iosevka monospace font built from source

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '../lib/css/main.css';
 import type { Metadata } from 'next';
 import { Poppins, Work_Sans } from 'next/font/google';
+import localFont from 'next/font/local';
 import { FC, ReactNode } from 'react';
 import background from '../../public/background.svg';
 import { NavigationBar } from '../components/server';
@@ -8,18 +9,27 @@ import { rootTitle } from '../lib/constants';
 import styles from './layout.module.css';
 
 // @ts-expect-error: Font loaders must be called and assigned to a const in the module scope
-const _poppins = Poppins({
+const _fontPrimary = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  style: ['normal', 'italic'],
+  weight: ['400', '500'],
+  style: 'normal',
   display: 'swap',
 });
 
 // @ts-expect-error: Font loaders must be called and assigned to a const in the module scope
-const _workSans = Work_Sans({
+const _fontSecondary = Work_Sans({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  style: ['normal', 'italic'],
+  weight: ['400', '500'],
+  style: 'normal',
+  display: 'swap',
+});
+
+// @ts-expect-error: Font loaders must be called and assigned to a const in the module scope
+const Iosevka = localFont({
+  src: '../lib/fonts/iosevka-medium.woff2',
+  declarations: [{ prop: 'fontFamily', value: 'Foo' }],
+  weight: '500',
+  style: 'normal',
   display: 'swap',
 });
 
