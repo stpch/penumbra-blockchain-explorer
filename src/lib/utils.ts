7cb868428622ea9cb7309230cd742b7b82d8cd99
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-09 16:14:27 +0100

    Refactor hash shortening

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,2 +1,2 @@
 export const shortenHash = (hash: string) =>
-  hash.length < 16 ? hash : `${hash.slice(0, 8)}...${hash.slice(-8)}`;
+  hash.length < 19 ? hash : `${hash.slice(0, 8)}...${hash.slice(-8)}`;
