6ce41087c61777563a23a2f54d02e35b8e426a79
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-09 18:57:43 +0100

    Merge dashboard into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
 import { FC } from 'react';
-import { Container, Dashboard } from '../components/server';
+import { BlockPanel, BurnPanel, TransactionPanel } from '../components/client';
+import { Container, LatestBlocksTable, LatestTransactionsTable } from '../components/server';
+import { blocks, transactions } from '../lib/mocks';
+import { shortenHash } from '../lib/utils';
+import styles from './page.module.css';
+
+const latestBlocks = blocks.slice(0, 10);
+
+const latestTransactions = transactions.slice(0, 10).map(transaction => ({
+  ...transaction,
+  hash: shortenHash(transaction.hash),
+}));
 
 const HomePage: FC = async () => {
   return (
     <Container>
-      <Dashboard />
+      <div className={styles.dashboard}>
+        <BlockPanel />
+        <TransactionPanel />
+        <BurnPanel />
+        <LatestBlocksTable blocks={latestBlocks} />
+        <LatestTransactionsTable transactions={latestTransactions} />
+      </div>
     </Container>
   );
 };
--- src/components/server/index.ts
+++ src/components/server/index.ts
@@ -1,5 +1,4 @@
 /* istanbul ignore file */
 export { default as Container } from './container';
-export { default as Dashboard } from './dashboard';
 export { default as NavigationBar } from './navigationBar';
 export * from './tables';
