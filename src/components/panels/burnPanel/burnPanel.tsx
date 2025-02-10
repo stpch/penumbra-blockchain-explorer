4efca323a22dda8ca18d1165635cd74555b3fa9d
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-10 03:07:58 +0100

    Implement burn panel chart

--- src/components/panels/burnPanel/burnPanel.tsx
+++ src/components/panels/burnPanel/burnPanel.tsx
@@ -21,7 +21,34 @@ const BurnPanel: FC<Props> = props => (
     number={86990}
     footer='Average'
   >
-    Burn panel
+    <div className={styles.chart}>
+      <div className={styles.bar} style={{ marginTop: 22, height: 32 }} />
+      <div className={styles.bar} style={{ marginTop: 18, height: 28 }} />
+      <div className={styles.bar} style={{ marginTop: 29, height: 20 }} />
+      <div className={styles.bar} style={{ marginTop: 29, height: 19 }} />
+      <div className={styles.bar} style={{ marginTop: 22, height: 32 }} />
+      <div className={styles.bar} style={{ marginTop: 29, height: 12 }} />
+      <div className={styles.bar} style={{ marginTop: 28, height: 26 }} />
+      <div className={styles.bar} style={{ marginTop: 12, height: 50 }} />
+      <div className={styles.bar} style={{ marginTop: 4, height: 58 }} />
+      <div className={styles.bar} style={{ marginTop: 18, height: 36 }} />
+      <div className={styles.bar} style={{ marginTop: 24, height: 21 }} />
+      <div className={styles.bar} style={{ marginTop: 12, height: 33 }} />
+      <div className={styles.bar} style={{ marginTop: 24, height: 30 }} />
+      <div className={styles.bar} style={{ marginTop: 12, height: 33 }} />
+      <div className={styles.bar} style={{ marginTop: 12, height: 42 }} />
+      <div className={styles.bar} style={{ marginTop: 19, height: 19 }} />
+      <div className={styles.bar} style={{ marginTop: 19, height: 19 }} />
+      <div className={styles.bar} style={{ marginTop: 19, height: 29 }} />
+      <div className={styles.bar} style={{ marginTop: 12, height: 36 }} />
+      <div className={styles.bar} style={{ height: 62 }} />
+      <div className={styles.bar} style={{ marginTop: 20, height: 27 }} />
+    </div>
+    <div className={styles.footer}>
+      <span>Jan 13</span>
+      <span>Jan 14</span>
+      <span>Today</span>
+    </div>
   </Panel>
 );
 
