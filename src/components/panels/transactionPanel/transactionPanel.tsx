0c85e01580b6833c8cabed767671681930dd7bc0
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-10 02:36:08 +0100

    Implement transaction panel chart

--- src/components/panels/transactionPanel/transactionPanel.tsx
+++ src/components/panels/transactionPanel/transactionPanel.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import Image from 'next/image';
 import { FC, ReactNode } from 'react';
+import { formatNumber } from '../../../lib/utils';
 import Panel from '../panel';
 import styles from './transactionPanel.module.css';
 import icon from './transactionPanelIcon.svg';
@@ -19,9 +20,111 @@ const TransactionPanel: FC<Props> = props => (
       </>
     }
     number={28057456}
-    footer='68,499 new today'
+    footer={`${formatNumber(68499)} new today`}
   >
-    Transaction panel
+    <div className={styles.chart}>
+      <div className={styles.verticalBarGroup}>
+        <div className={styles.bar} style={{ height: 7 }} />
+        <div className={clsx(styles.bar, styles.full)} style={{ height: 22 }} />
+      </div>
+      <div className={styles.horizontalBarGroup}>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 12 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 22 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 10 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 18 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 11 }} />
+        </div>
+      </div>
+      <div className={styles.horizontalBarGroup}>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 28 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 56 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 7 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 22 }} />
+        </div>
+      </div>
+      <div className={styles.horizontalBarGroup}>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 51 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 33 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 10 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 37 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 13 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 58 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 11 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 32 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 10 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 26 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 7 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 22 }} />
+        </div>
+      </div>
+      <div className={styles.horizontalBarGroup}>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 24 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 41 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 41 }} />
+        </div>
+      </div>
+      <div className={styles.horizontalBarGroup}>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 11 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 7 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 22 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 10 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 18 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={styles.bar} style={{ height: 12 }} />
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 22 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 41 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 11 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 22 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 18 }} />
+        </div>
+        <div className={styles.verticalBarGroup}>
+          <div className={clsx(styles.bar, styles.full)} style={{ height: 22 }} />
+        </div>
+      </div>
+    </div>
+    <div className={styles.footer}>
+      <span>12</span>
+      <span>16</span>
+      <span>20</span>
+    </div>
   </Panel>
 );
 
