eb007dd423eaab1f980470160cdad23c0c92201f
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-11 00:50:55 +0100

    Disable CSS chunking entirely

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    // Fixes CSS order: https://nextjs.org/docs/app/api-reference/config/next-config-js/cssChunking
-    cssChunking: 'strict',
+    cssChunking: false,
   },
 };
 
