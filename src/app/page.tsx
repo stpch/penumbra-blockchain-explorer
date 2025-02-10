3cfd256f0315cc5889a7acf64b7f4ab3969faba3
Stéphane Chorne <stephane.chorne@gmail.com>
2025-02-10 04:30:02 +0100

    Add home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,23 +16,28 @@ const latestTransactions = transactions.slice(0, 10);
 
 const HomePage: FC = async () => {
   return (
-    <Container>
-      <div className={styles.dashboard}>
-        <BlockPanel />
-        <TransactionPanel />
-        <BurnPanel />
-        <BlockTable
-          blocks={latestBlocks}
-          title='Latest blocks'
-          actions={<Button href='/blocks'>View all</Button>}
-        />
-        <TransactionTable
-          transactions={latestTransactions}
-          title='Latest transactions'
-          actions={<Button href='/transactions'>View all</Button>}
-        />
-      </div>
-    </Container>
+    <>
+      <Container>
+        <h1 className={styles.title}>Penumbra Blockchain Explorer</h1>
+      </Container>
+      <Container>
+        <div className={styles.dashboard}>
+          <BlockPanel />
+          <TransactionPanel />
+          <BurnPanel />
+          <BlockTable
+            blocks={latestBlocks}
+            title='Latest blocks'
+            actions={<Button href='/blocks'>View all</Button>}
+          />
+          <TransactionTable
+            transactions={latestTransactions}
+            title='Latest transactions'
+            actions={<Button href='/transactions'>View all</Button>}
+          />
+        </div>
+      </Container>
+    </>
   );
 };
 
